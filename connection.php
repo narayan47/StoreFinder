24ce5c159e465f74988d2d328cf9950f97d873ad
Narayan Charan <narayancharan865@gmail.com>
2026-04-08 15:35:37 +0530

    Update database connection parameters

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-    $server="localhost";
-    $user="root";
-    $password="";
-    $database="db_storefinder";
+    $server="your_server";
+    $user="user_name";
+    $password="password";
+    $database="databasename";
     $conn=new mysqli($server, $user, $password, $database);
     if($conn->connect_error){
         echo "Connection Fail";
